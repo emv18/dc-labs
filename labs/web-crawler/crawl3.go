fcabb5d2a2ce63c1d48cc0da1a5379a2cbeb7c11
Eduardo Morales Vizcarra <0226265@up.edu.mx>
2021-04-27 03:23:03 +0000

    My attempt to commit

--- labs/web-crawler/crawl3.go
+++ labs/web-crawler/crawl3.go
@@ -22,6 +22,29 @@ import (
 // enforce a limit of 20 concurrent requests.
 var tokens = make(chan struct{}, 20)
 
+func createFile(name string, urls []string) {
+	f, err := os.Create(name)
+	if err != nil {
+		fmt.Println("File creation failed")
+		return
+	}
+	for _,url := range len(urls){
+		f.Write(url+"\n")
+	}
+	defer f.Close()
+}
+
+func readParam() {
+	args := os.Args[1:]
+	var url string
+	var depth int
+
+	url = args[3]
+	number = args[2].split('=')
+	depth = number[2]
+	return depth
+}
+
 func crawl(url string) []string {
 	fmt.Println(url)
 	tokens <- struct{}{} // acquire a token
@@ -47,6 +70,9 @@ func main() {
 
 	// Crawl the web concurrently.
 	seen := make(map[string]bool)
+
+	depth := readParam()
+	urls []string
 	for ; n > 0; n-- {
 		list := <-worklist
 		for _, link := range list {
@@ -56,9 +82,11 @@ func main() {
 				go func(link string) {
 					worklist <- crawl(link)
 				}(link)
+				append(urls, link)
 			}
 		}
 	}
+	createFile(UrlsList, urls)
 }
 
 //!-
